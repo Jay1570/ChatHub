6c473fbd465da366999982660785e27cb44f89db
Jay Gheewala <jaygheewala157@gmail.com>
2024-06-29 10:38:42 +0530

    Updated Dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,8 +4,8 @@ plugins {
     alias(libs.plugins.jetbrains.kotlin.android) apply false
     id("com.google.devtools.ksp") version "2.0.0-1.0.21" apply false
     id("com.google.dagger.hilt.android") version "2.51.1" apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false
-    id("com.google.firebase.crashlytics") version "2.9.9" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
+    id("com.google.firebase.crashlytics") version "3.0.2" apply false
     id("com.google.firebase.firebase-perf") version "1.4.2" apply false
     alias(libs.plugins.compose.compiler) apply false
 }
